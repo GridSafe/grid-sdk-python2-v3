adfdad8e10b33fc61927979d94b919c3f1b3800b
jinxingxing <jxinging@gmail.com>
2015-07-31 15:57:28 +0800

    update README.md

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
-CDNZZ APIv3 Python2 SDK
+速致 CDN PHP SDK APIv3
 =======================
+使用示例参考: tests.py
+API文档 https://docs.cdnzz.com/dev/api-v3/
+
--- cdnzz/settings.py
+++ cdnzz/settings.py
@@ -13,6 +13,7 @@ USER_AGENT = "CDNZZ-SDK/3.0; Python%s.%s; %s" % (
     sys.version_info.major, sys.version_info.minor, platform.system())
 
 API_URL = 'https://www.cdnzz.com/apiv3/json'
+API_URL = 'http://127.0.0.1:18888/apiv3/json'
 
 INVALID_TOKEN_ERROR = 10260
 
